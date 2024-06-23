0f81fad47abcc66b2d52384951487e5e49afd8db
usama387 <128306049+usama387@users.noreply.github.com>
2024-06-23 17:16:21 +0500

    Auth functions and routes added in Navbar and is yet to be connected with sign-in route

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -2,6 +2,13 @@ import Link from "next/link";
 import React from "react";
 import MobileMenu from "./MobileMenu";
 import Image from "next/image";
+import {
+  ClerkLoaded,
+  ClerkLoading,
+  SignedIn,
+  SignedOut,
+  UserButton,
+} from "@clerk/nextjs";
 
 const Navbar = () => {
   return (
@@ -53,6 +60,35 @@ const Navbar = () => {
 
       {/* RIGHT SIDE */}
       <div className="w-[30%] flex items-center gap-4 xl:gap-8 justify-end">
+        {/* When Clerk is being loaded render a spinner */}
+        <ClerkLoading>
+          <div className="inline-block h-4 w-4 animate-spin rounded-full border-2 border-gray-500 border-solid border-current border-e-transparent align-[-0.125em] text-surface motion-reduce:animate-[spin_1.5s_linear_infinite] dark:text-white" />
+        </ClerkLoading>
+
+        {/* When Clerk is loaded */}
+        <ClerkLoaded>
+          {/* When Authenticated */}
+          <SignedIn>
+            <div className="cursor-pointer">
+              <Image src="/people.png" alt="" width={20} height={20} />
+            </div>
+            <div className="cursor-pointer">
+              <Image src="/messages.png" alt="" width={20} height={20} />
+            </div>
+            <div className="cursor-pointer">
+              <Image src="/notification.png" alt="" width={20} height={20} />
+            </div>
+            <UserButton />
+          </SignedIn>
+
+          {/* If not authenticated */}
+          <SignedOut>
+            <div className="flex items-center gap-2 text-sm">
+              <Image src="/login.png" alt="" width={20} height={20} />
+              <Link href="/sign-in">Login/Register</Link>
+            </div>
+          </SignedOut>
+        </ClerkLoaded>
         <MobileMenu />
       </div>
     </div>
