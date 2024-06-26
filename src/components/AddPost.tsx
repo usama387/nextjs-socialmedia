4dffa6bf4735d527a9e7f5859f8088b7166c68cb
usama387 <128306049+usama387@users.noreply.github.com>
2024-06-26 12:31:13 +0500

    AddPost component design completed a child of homepage

--- src/components/AddPost.tsx
+++ src/components/AddPost.tsx
@@ -1,7 +1,56 @@
+import Image from "next/image";
 import React from "react";
 
+// Child component of homepage
 const AddPost = () => {
-  return <div>AddPost</div>;
+  return (
+    <div className="p-4 shadow-md bg-white rounded-lg flex gap-4 justify-between text-sm">
+      {/* Avatar */}
+      <Image
+        src="https://images.pexels.com/photos/20035451/pexels-photo-20035451/free-photo-of-buildings-around-cobblestone-street-in-city.jpeg?auto=compress&cs=tinysrgb&w=600&lazy=load"
+        width={48}
+        height={48}
+        alt=""
+        className="w-12 h-12 object-cover rounded-full "
+      />
+      {/* Post */}
+      <div className="flex-1">
+        {/* text input  */}
+        <div className="flex gap-4">
+          <textarea
+            placeholder="what's on your mind?"
+            className="flex-1 bg-slate-100 rounded-lg p-2"
+          ></textarea>
+          <Image
+            src="/emoji.png"
+            width={20}
+            height={20}
+            alt=""
+            className="w-5 h-5 cursor-pointer self-end"
+          />
+        </div>
+        {/* post options */}
+        <div className="flex items-center gap-4 mt-4 text-gray-400 flex-wrap">
+          <div className="flex items-center gap-2 cursor-pointer">
+            <Image src="/addimage.png" width={20} height={20} alt="" />
+            Photo
+          </div>
+          <div className="flex items-center gap-2 cursor-pointer">
+            <Image src="/addVideo.png" width={20} height={20} alt="" />
+            Video
+          </div>
+          <div className="flex items-center gap-2 cursor-pointer">
+            <Image src="/poll.png" width={20} height={20} alt="" />
+            Poll
+          </div>
+          <div className="flex items-center gap-2 cursor-pointer">
+            <Image src="/addevent.png" width={20} height={20} alt="" />
+            Event
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default AddPost;
