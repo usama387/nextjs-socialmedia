4a558df13d70fdb425ec8510277e772f54f62d54
usama387 <128306049+usama387@users.noreply.github.com>
2024-06-27 12:54:59 +0500

    PostFeed where all posts appear design added

--- src/components/PostFeed.tsx
+++ src/components/PostFeed.tsx
@@ -1,7 +1,15 @@
 import React from "react";
+import Post from "./Post";
 
+// child of homepage
 const PostFeed = () => {
-  return <div>PostFeed</div>;
+  return <div className="p-4 shadow-md bg-white rounded-lg flex flex-col gap-12">
+    <Post />
+    <Post />
+    <Post />
+    <Post />
+    <Post />
+  </div>;
 };
 
 export default PostFeed;
