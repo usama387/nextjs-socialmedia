ed62804384373d8fac5e9cbab91cdaf5faf4a975
usama387 <128306049+usama387@users.noreply.github.com>
2024-06-27 16:17:33 +0500

    FriendRequests component design added a child of RightMenu

--- src/components/RightMenu.tsx
+++ src/components/RightMenu.tsx
@@ -1,7 +1,17 @@
-import React from "react";
+import Advertisement from "./Advertisement";
+import Birthdays from "./Birthdays";
+import FriendRequests from "./FriendRequests";
 
-const RightMenu = () => {
-  return <div>RightMenu</div>;
+// If userId is passed as props it displays specific details of that user
+const RightMenu = ({ userId }: { userId?: string }) => {
+  return (
+    <div className="flex flex-col gap-6">
+      <FriendRequests />
+      <Birthdays />
+      {/* passing "md" as size prop */}
+      <Advertisement size="md" />
+    </div>
+  );
 };
 
 export default RightMenu;
