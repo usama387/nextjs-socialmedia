18d3de16af2eb0d579a5a8a0913a76e6b9ef2acd
usama387 <128306049+usama387@users.noreply.github.com>
2024-07-04 15:04:00 +0500

    Fetched logged in user details on ProfileCard component a child of LeftMenu component

--- src/components/ProfileCard.tsx
+++ src/components/ProfileCard.tsx
@@ -1,18 +1,44 @@
+import prisma from "@/lib/PrismaClient";
+import { auth } from "@clerk/nextjs/server";
 import Image from "next/image";
 
 // child of LeftMenu component
-const ProfileCard = () => {
+const ProfileCard = async () => {
+  // getting access to logged in user
+  const { userId } = auth();
+
+  // if user not found
+  if (!userId) return null;
+
+  // from the user table fetching user detail and it's number of followers
+  const user = await prisma.user.findFirst({
+    where: {
+      id: userId,
+    },
+    include: {
+      _count: {
+        select: {
+          followers: true,
+        },
+      },
+    },
+  });
+
+  console.log(user);
+  // if no user no found
+  if (!user) return null;
+
   return (
     <div className="p-4 bg-white rounded-lg shadow-md text-sm flex flex-col gap-6">
       <div className="h-20 relative">
         <Image
-          src="https://images.pexels.com/photos/26546097/pexels-photo-26546097/free-photo-of-a-goat-lying-outdoors.jpeg?auto=compress&cs=tinysrgb&w=600&lazy=load"
+          src={user?.cover || "/noCover.png"}
           alt=""
           fill
           className="rounded-md object-cover"
         />
         <Image
-          src="https://images.pexels.com/photos/26600378/pexels-photo-26600378/free-photo-of-cuidar-la-vida.jpeg?auto=compress&cs=tinysrgb&w=600&lazy=load"
+          src={user?.avatar || "/noAvatar.png"}
           alt=""
           width={48}
           height={48}
@@ -20,7 +46,12 @@ const ProfileCard = () => {
         />
       </div>
       <div className="h-20 flex flex-col gap-2 items-center">
-        <span className="font-semibold">Usama Razaaq</span>
+        <span className="font-semibold">
+          {/* Rendering of username with Ternary Operator */}
+          {user.name && user.surname
+            ? user.name + "" + user.surname
+            : user.username}
+        </span>
         <div className="flex items-center gap-4">
           <div className="flex">
             <Image
@@ -45,7 +76,10 @@ const ProfileCard = () => {
               className="rounded-full w-3 h-3"
             />
           </div>
-          <span className="text-xs text-gray-500">500 Followers</span>
+          {/* Number of followers user has */}
+          <span className="text-xs text-gray-500">
+            {user._count.followers} Followers
+          </span>
         </div>
         <button className="bg-blue-500 text-white text-xs p-2 rounded-md">
           Mt Profile
