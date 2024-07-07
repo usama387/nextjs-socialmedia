88e5b4d3f18bcc03f9db947f77fd4b72258192fd
usama387 <128306049+usama387@users.noreply.github.com>
2024-07-07 10:42:16 +0500

    User details made dynamic coming as props from RightMenu component

--- src/components/RightMenu.tsx
+++ src/components/RightMenu.tsx
@@ -1,17 +1,19 @@
+import { User } from "@prisma/client";
 import Advertisement from "./Advertisement";
 import Birthdays from "./Birthdays";
 import FriendRequests from "./FriendRequests";
 import UserInformationCard from "./UserInformationCard";
 import UserMediaCard from "./UserMediaCard";
 
-// If userId is passed as props it displays specific details of that user
-const RightMenu = ({ userId }: { userId?: string }) => {
+// If user is passed as props from profile route it displays specific details of that user
+const RightMenu = ({ user }: { user?: User }) => {
   return (
     <div className="flex flex-col gap-6">
-      {userId ? (
+      {/* when user is available pass it down to following child component */}
+      {user ? (
         <>
-          <UserInformationCard userId={userId} />
-          <UserMediaCard userId={userId} />
+          <UserInformationCard user={user} />
+          <UserMediaCard user={user} />
         </>
       ) : null}
       <FriendRequests />
--- src/components/UserInformationCard.tsx
+++ src/components/UserInformationCard.tsx
@@ -1,8 +1,19 @@
+import { User } from "@prisma/client";
 import Image from "next/image";
 import Link from "next/link";
 
 // Child of RightMenu component
-const UserInformationCard = ({ userId }: { userId: string }) => {
+const UserInformationCard = ({ user }: { user: User }) => {
+  // formatting user joining date
+  const createdAtDate = new Date(user.createdAt);
+
+  // transforming it into presentable time format
+  const formattedDate = createdAtDate.toLocaleDateString("en-US", {
+    year: "numeric",
+    month: "long",
+    day: "numeric",
+  });
+
   return (
     <div className="p-4 bg-white rounded-lg shadow-md text-sm flex flex-col gap-4">
       {/* TOP WITH HEADINGS*/}
@@ -15,48 +26,67 @@ const UserInformationCard = ({ userId }: { userId: string }) => {
       {/* BOTTOM WITH USER DETAILS*/}
       <div className="flex flex-col gap-4 text-gray-500">
         <div className="flex items-center gap-2">
-          <span className="text-xl text-black">John Wick</span>
-          <span className="text-sm">@Wick</span>
-        </div>
-        <p>
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Incidunt,
-          voluptatum?
-        </p>
-        <div className="flex items-center gap-2">
-          <Image src="/map.png" alt="" height={16} width={16} />
-          <span>
-            Living in <b>Karachi</b>
+          <span className="text-xl text-black">
+            {" "}
+            {user.name && user.surname
+              ? user.name + "" + user.surname
+              : user.username}
           </span>
+          <span className="text-sm">@{user.username}</span>
         </div>
-        <div className="flex items-center gap-2">
-          <Image src="/school.png" alt="" height={16} width={16} />
-          <span>
-            Went to <b>Iqra University</b>
-          </span>
-        </div>
-        <div className="flex items-center gap-2">
-          <Image src="/work.png" alt="" height={16} width={16} />
-          <span>
-            Works at <b>IBC</b>
-          </span>
-        </div>
-        <div className="flex items-center justify-between">
-          <div className="flex gap-1 items-center">
-            <Image src="/link.png" alt="" height={16} width={16} />
-            <Link
-              href="https://Usama.dev"
-              className="text-blue-500 font-medium"
-            >
-              Usama.dev
-            </Link>
+        {/* when description is available */}
+        {user.description && <p>{user.description}</p>}
+        {/* when city Information is available */}
+        {user.city && (
+          <div className="flex items-center gap-2">
+            <Image src="/map.png" alt="" height={16} width={16} />
+            <span>
+              Living in <b>{user.city}</b>
+            </span>
           </div>
+        )}
+        {/* When school Information is available */}
+        {user.school && (
+          <div className="flex items-center gap-2">
+            <Image src="/school.png" alt="" height={16} width={16} />
+            <span>
+              Went to <b>{user.school}</b>
+            </span>
+          </div>
+        )}
+        {/* when work place information is available */}
+        {user.work && (
+          <div className="flex items-center gap-2">
+            <Image src="/work.png" alt="" height={16} width={16} />
+            <span>
+              Works at <b>{user.work}</b>
+            </span>
+          </div>
+        )}
+        {/* when website information is available */}
+        <div className="flex items-center justify-between">
+          {user.website && (
+            <div className="flex gap-1 items-center">
+              <Image src="/link.png" alt="" height={16} width={16} />
+              <Link href={user.website} className="text-blue-500 font-medium">
+                {user.website}
+              </Link>
+            </div>
+          )}
+
+          {/* if joining date is available */}
           <div className="flex gap-1 items-center">
             <Image src="/date.png" alt="" height={16} width={16} />
-            <span>Joined November 2024</span>
+            {/* using it here  */}
+            <span>Joined {formattedDate}</span>
           </div>
         </div>
-        <button className="bg-blue-500 text-white text-sm rounded-md p-2">Follow</button>
-        <span className="text-red-400 self-end text-xs cursor-pointer">Block User</span>
+        <button className="bg-blue-500 text-white text-sm rounded-md p-2">
+          Follow
+        </button>
+        <span className="text-red-400 self-end text-xs cursor-pointer">
+          Block User
+        </span>
       </div>
     </div>
   );
