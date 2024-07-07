5048acce0d6797d7b6d95219bfe3375d3dae9522
usama387 <128306049+usama387@users.noreply.github.com>
2024-07-07 11:13:59 +0500

    Paths fixed and added React Suspense in top two components

--- src/components/RightMenu.tsx
+++ src/components/RightMenu.tsx
@@ -1,9 +1,10 @@
 import { User } from "@prisma/client";
 import Advertisement from "./Advertisement";
-import Birthdays from "./Birthdays";
 import FriendRequests from "./FriendRequests";
-import UserInformationCard from "./UserInformationCard";
-import UserMediaCard from "./UserMediaCard";
+import { Suspense } from "react";
+import UserInformationCard from "./RightMenu/UserInformationCard";
+import UserMediaCard from "./RightMenu/UserMediaCard";
+import Birthdays from "./RightMenu/Birthdays";
 
 // If user is passed as props from profile route it displays specific details of that user
 const RightMenu = ({ user }: { user?: User }) => {
@@ -12,8 +13,12 @@ const RightMenu = ({ user }: { user?: User }) => {
       {/* when user is available pass it down to following child component */}
       {user ? (
         <>
-          <UserInformationCard user={user} />
-          <UserMediaCard user={user} />
+          <Suspense fallback="loading...">
+            <UserInformationCard user={user} />
+          </Suspense>
+          <Suspense fallback="loading...">
+            <UserMediaCard user={user} />
+          </Suspense>
         </>
       ) : null}
       <FriendRequests />
