4b5e67a2e35dcc20bc2b56c4a1c3063722a0d90c
usama387 <128306049+usama387@users.noreply.github.com>
2024-07-16 17:39:18 +0500

    Used my server actions in FriendRequestList component along with useOptimistic hook to accept or decline follow request and update ui immediately

--- src/components/RightMenu/FriendRequestList.tsx
+++ src/components/RightMenu/FriendRequestList.tsx
@@ -1,8 +1,9 @@
 "use client";
 
+import { acceptFollowRequest, declineFollowRequest } from "@/lib/actions";
 import { FollowRequest, User } from "@prisma/client";
 import Image from "next/image";
-import { useState } from "react";
+import { useOptimistic, useState } from "react";
 
 // type safety of request props using the db tables
 type RequestWithUser = FollowRequest & {
@@ -11,9 +12,44 @@ type RequestWithUser = FollowRequest & {
 
 // child of FriendRequest component taking request as array props
 const FriendRequestList = ({ requests }: { requests: RequestWithUser[] }) => {
+  // to manage state of requests
+  const [requestState, setRequestState] = useState(requests);
+
+  // function to accept the request taking two parameters
+  const acceptRequest = async (requestId: number, userId: string) => {
+    removeOptimisticRequest(requestId);
+    try {
+      await acceptFollowRequest(userId);
+
+      // now updating the main state when request is accepted
+      setRequestState((prev) => prev.filter((req) => req.id !== requestId));
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  // function to accept the request taking two parameters
+  const declineRequest = async (requestId: number, userId: string) => {
+    removeOptimisticRequest(requestId);
+    try {
+      await declineFollowRequest(userId);
+
+      // now updating the main state when request is accepted
+      setRequestState((prev) => prev.filter((req) => req.id !== requestId));
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  // here i pass the main state of my useState to the optimistic hook
+  const [optimisticRequest, removeOptimisticRequest] = useOptimistic(
+    requestState,
+    (state, value: number) => state.filter((req) => req.id !== value)
+  );
+
   return (
     <div>
-      {requests.map((request) => (
+      {optimisticRequest.map((request) => (
         <div className="flex items-center justify-between" key={request.id}>
           <div className="flex items-center gap-4">
             <Image
@@ -30,20 +66,28 @@ const FriendRequestList = ({ requests }: { requests: RequestWithUser[] }) => {
             </span>
           </div>
           <div className="flex gap-3 justify-end">
-            <Image
-              src="/accept.png"
-              alt=""
-              width={20}
-              height={20}
-              className="cursor-pointer"
-            />
-            <Image
-              src="/reject.png"
-              alt=""
-              width={20}
-              height={20}
-              className="cursor-pointer"
-            />
+            <form action={() => acceptRequest(request.id, request.sender.id)}>
+              <button>
+                <Image
+                  src="/accept.png"
+                  alt=""
+                  width={20}
+                  height={20}
+                  className="cursor-pointer"
+                />
+              </button>
+            </form>
+            <form action={() => declineRequest(request.id, request.sender.id)}>
+              <button>
+                <Image
+                  src="/reject.png"
+                  alt=""
+                  width={20}
+                  height={20}
+                  className="cursor-pointer"
+                />
+              </button>
+            </form>
           </div>
         </div>
       ))}
